ca4c101ab0dc11d5d0e726cab3e84dd8fe9b3eac
ktmwlf123 <45546951+ktmwlf123@users.noreply.github.com>
2024-05-08 19:42:47 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+let pData = [
+    {"index": "0", "ai":0.856, "xai":0.743, "texture":0.165, "vessel":0.825, "color":0.578},
+    {"index": "1","ai":0.796, "xai":0.164, "texture":0.164, "vessel":0.167, "color":0.165},
+    {"index": "2", "ai":0.996, "xai":0.553, "texture":0.556, "vessel":0.889, "color":0.666},
+    {"index": "3", "ai":0.543, "xai":0.431, "texture":0.843, "vessel":0.164, "color":0.479},
+    {"index": "4", "ai":0.864, "xai":0.154, "texture":0.316, "vessel":0.983, "color":0.484},
+    {"index": "5", "ai":0.991, "xai":0.467, "texture":0.184, "vessel":0.167, "color":0.273},
+    {"index": "6", "ai":0.843, "xai":0.781, "texture":0.741, "vessel":0.843, "color":0.788},
+    {"index": "7", "ai":-0.552, "xai":-0.413, "texture":-0.134, "vessel":-0.423, "color":-0.323},
+    {"index": "8", "ai":-0.752, "xai":-0.661, "texture":-0.168, "vessel":-0.612, "color":-0.480},
+    {"index": "9", "ai":0.621, "xai":0.168, "texture":0.553, "vessel":0.881, "color":0.534}
+]
